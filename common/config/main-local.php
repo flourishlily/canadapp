b5e5bd13266b919e8a492d29185ea614d49440f7
flourishlily <flourishlily@gmail.com>
2018-05-06 22:09:37 +0800

    start up version

---
+++ common/config/main-local.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+	    'emulatePrepare'=>true,
+            'dsn' => 'mysql:host=localhost;dbname=canadapp',
+            'username' => 'root',
+            'password' => 'powerful',
+            'charset' => 'utf8',
+		
+        ],
+       /* 'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],*/
+    ],
+];
